cf0f976561fbeaba9648d2d40494d05f87c4e960
kaiser shahid <kaisershahid@gmail.com>
2020-07-14 09:29:14 -0700

    -- removing null values from map

--- dashboard.tf
+++ dashboard.tf
@@ -1,23 +1,14 @@
 locals {
   dashboards = var.dashboards == null ? {} : var.dashboards
-  #  dashboards = { for key, dashboard in local.dashboards_prep:
-  #    key => merge(dashboard, {
-  #    widgets = [ for widget in lookup(dashboard, "widgets", []):
-  #    merge(widget, {
-  #      metadata = merge(lookup(widget, "metadata", {}), {
-  ##        id = lookup(widget, "id")
-  ##        expression = lookup(widget, "expression", null)
-  #      })
-  #    })
-  #    ]
-  #  })
-  #  }
 }
 
 resource "aws_cloudwatch_dashboard" "dashboard" {
   for_each       = local.dashboards
   dashboard_name = lookup(each.value, "name")
-  dashboard_body = jsonencode({
+
+  # cloudwatch hates null values in map, so we need to strip them out of JSON
+  # (tf doesn't have a function to remove null elements from maps)
+  dashboard_body = replace(jsonencode({
     widgets = [
     for widget in lookup(each.value, "widgets", [ ]):
     {
@@ -26,7 +17,6 @@ resource "aws_cloudwatch_dashboard" "dashboard" {
       y          = lookup(widget, "y")
       width      = lookup(widget, "width")
       height     = lookup(widget, "height")
-      stat       = lookup(widget, "statistic", null)
       properties = merge(lookup(widget, "properties", {}), {
         # converting the dimensions key-value map into a list of alternating
         # key & value elements requires us to
@@ -41,28 +31,28 @@ resource "aws_cloudwatch_dashboard" "dashboard" {
         #
         # @formatter:off
         metrics = [ for metric in lookup(widget, "metrics", [ ]):
-          flatten(concat([
-            # for expressions, these would produce `["", ""]`, which can be reduced to []
-            compact([
-              lookup(metric, "namespace", ""),
-              lookup(metric, "metric", "")
-            ]),
-            flatten([ for key, val in lookup(metric, "dimensions", {}): [ key, val ] ]),
-            # auto-inject id and expression for metric properties
-            # for stat, we basically set as follows: metadata.stat, metric.statistic, null (defer to properties.stat)
-            [ merge(
-#                { stat = lookup(metric, "statistic", null) },
-                lookup(metric, "metadata", {}),
-                {
-                  id         = lookup(metric, "id")
-                  expression = lookup(metric, "expression", null)
-                }
-            ) ]
-          ]))
+        flatten(concat([
+          # for expressions, these would produce `["", ""]`, which can be reduced to []
+          compact([
+            lookup(metric, "namespace", ""),
+            lookup(metric, "metric", "")
+          ]),
+          flatten([ for key, val in lookup(metric, "dimensions", {}): [ key, val ] ]),
+          # auto-inject id and expression for metric properties
+          # for stat, we basically set as follows: metadata.stat, metric.statistic, null (defer to properties.stat)
+          [ merge(
+          #                { stat = lookup(metric, "statistic", null) },
+          lookup(metric, "metadata", {}),
+          {
+            id         = lookup(metric, "id")
+            expression = lookup(metric, "expression", null)
+          }
+          ) ]
+        ]))
         ]
         # @formatter:on
       })
     }
     ]
-  })
+  }), "/\"[^\"]+\":null,?/", "")
 }
